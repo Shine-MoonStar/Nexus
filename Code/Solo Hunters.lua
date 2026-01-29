2462622e07f968bdb167b5fc14b791125c9b65a8
Shine-MoonStar <3977373683@qq.com>
2026-01-29 19:41:21 +0800

    Create Solo Hunters.lua

---
+++ Code/Solo Hunters.lua
@@ -0,0 +1,11 @@
+return = {
+    ["40KCCUTHANKS66"] = "Free Rewards",
+    ["TOTHEMOON"] = "Stat Reset",
+    ["LOVETOBRAZIL"] = "Stat Reset",
+    ["35KCCUTHANKS33"] = "Free Rewards",
+    ["THANKSFORTHELIKESGUYS"] = "Free Rewards",
+    ["WWWW"] = "200 Gems",
+    ["RELEASE"] = "Class Reroll",
+    ["RESETMYSTATS"] = "SP Reset",
+    ["CLASSREROLL"] = "Class Reroll"
+}
